4d01c84b749682c02229d0671c5a9a3b80d722d0
zhwysh@outlook.com <zhwysh@outlook.com>
2018-08-02 17:41:29 +0100

    self-signed signature not match

--- python-sdk/test.py
+++ python-sdk/test.py
@@ -1,4 +1,3 @@
-
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
 from Crypto.Hash import SHA256
@@ -6,11 +5,39 @@ import base64
 
 import json
 import requests
+import os
 
 # generate a rsa key object
 rsa_object = RSA.generate(1024)
+
+# store the public and private key for this rsa object
+
+
+def renew_key(filename):
+    try:
+        os.remove(filename)
+    except:
+        print(f'No file found, create file {filename}')
+
+
+renew_key('public.pem')
+renew_key('private.pem')
+
+
+f1 = open('public.pem', 'wb')
+f2 = open('private.pem', 'wb')
+f1.write(rsa_object.publickey().exportKey())
+f2.write(rsa_object.exportKey())
+f1.close
+f2.close
+
+
+
+
 # print(rsa_object.publickey().exportKey())
-# print(rsa_object.exportKey())
+# print(str(rsa_object.exportKey().decode('utf-8')))
+# print(str(rsa_object.exportKey()).lstrip())
+# print(str(rsa_object.exportKey().decode('utf-8')))
 
 # create signature object with private key
 sig_object = PKCS1_v1_5.new(rsa_object)
@@ -18,6 +45,22 @@ sig_object = PKCS1_v1_5.new(rsa_object)
 # create a hash object based on SHA256
 hash_object = SHA256.new()
 
+message_object = {
+    "$namespace" : "default",
+    "$contract" : "onboard",
+    "$i" : {
+      "identity" : {
+        "publicKey" : str.encode(json.dumps()),
+        "type" : "secp256k1"
+      }
+    }
+} 
+
+# print(type(json.dumps(message_object)))
+print(json.dumps(message_object))
+hash_object.update(str.encode(json.dumps(message_object)))
+
+# print(type(str.encode('aaa')))
 # return a byte string signature
 signature = sig_object.sign(hash_object)
 
@@ -38,26 +81,32 @@ onboard_message = {
     "$namespace" : "default",
     "$contract" : "onboard",
     "$i" : {
-      "Jialin" : {
-        "publicKey" : str(rsa_object.publickey().exportKey()),
-        "type" : "secp256k1"
+      "identity" : {
+        "publicKey" : str(rsa_object.publickey().exportKey().decode('utf-8')),
+        "type" : "rsa"
       }
     }
   },
   "$selfsign" : True,
   "$sigs" : {
-    "identity" : str(signature_str)
+    "identity" : str(signature_str.decode('utf-8'))
   }
 }
 # print(str(signature_str))
 # print(type(str(signature_str)))
 
-# print(json.dumps(onboard_message))
+print(type(json.dumps(onboard_message)))
 
-r = requests.post('http://35.195.221.172:5260', json = [json.dumps(onboard_message)])
-# r.content()
-print(str(r._content))
-print(r.status_code)
+headers = {'Content-type': 'application/json'}
+r = requests.post('http://35.195.221.172:5260', data = json.dumps(onboard_message), headers = headers)
+# print(r.status_code)
+# print(r._content)
+# url = "http://localhost:8080"
+# data = {'sender': 'Alice', 'receiver': 'Bob', 'message': 'We did it!'}
+# r = requests.post(url, data=json.dumps(data), headers=headers)
+
+print(r._content)
+# print(r.status_code)
 
 
 # }

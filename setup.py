2dbc632addaeb507e13177b23611482d3b36a61a
jialin-yu <zhwysh@outlook.com>
2018-08-02 09:50:21 +0100

    inital file

---
+++ setup.py
@@ -0,0 +1,15 @@
+'''
+This is the setup file in python
+'''
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Activeledger-python-sdk',
+      version='1.0',
+      descrption='Python sdk for activeledger',
+      author='Activeldger',
+      )
+
+if __main__ == '__init__':
+    
